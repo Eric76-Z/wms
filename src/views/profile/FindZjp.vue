3d6b3e39b53533d55539a625bb06facc7aec03ac
hasaiii <hasaii1@outlook.com>
2021-09-10 23:36:32 +0800

    image

--- src/views/profile/FindZjp.vue
+++ src/views/profile/FindZjp.vue
@@ -1,121 +1,27 @@
 <template>
-  <div id="login">
-    <van-image
-      round
-      width="8rem"
-      height="8rem"
-      lazy-load
-      src="https://img01.yzcdn.cn/vant/cat.jpeg"
-      v-show="haveIcon"
-    >
-      <template v-slot:loading>
-        <van-loading type="spinner" size="20" />
-      </template>
-    </van-image>
-    <van-icon
-      class="iconfont"
-      class-prefix="icon"
-      name="weidenglu"
-      v-show="!haveIcon"
-    />
-    <van-form @submit="onSubmit">
-      <van-cell-group inset>
-        <van-field
-          v-model="state.username"
-          name="username"
-          label="用户名"
-          placeholder="用户名"
-          :rules="[{ required: true, message: '请填写用户名' }]"
-        />
-        <van-field
-          v-model="state.password"
-          type="password"
-          name="password"
-          label="密码"
-          placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
-        />
-      </van-cell-group>
-      <div style="margin: 16px">
-        <van-button round block type="success" native-type="submit">
-          登陆
-        </van-button>
-        <div class="halfbtn">
-          <van-button type="success" to="/register">注册</van-button>
-          <van-button type="warning" to="/resetpwd">重置密码</van-button>
-        </div>
-      </div>
-    </van-form>
+  <div id="findzjp">
+    <van-image fit="contain" :src="require('../../assets/img/wechat.jpg')" />
   </div>
 </template>
 
 <script>
-import { reactive, toRef, computed } from "vue";
-// import { useRouter } from "vue-router";
-import { useStore } from "vuex";
+// import { reactive, toRef, computed } from "vue";
+// // import { useRouter } from "vue-router";
+// import { useStore } from "vuex";
 
 export default {
-  name: "Login",
+  name: "FindZjp",
   setup() {
-    const store = useStore();
-    const navbarcfg = toRef(store.state, "navbarcfg");
-    navbarcfg.value.mainnavbarcfg = {
-      title: "用户登录",
-      isShow: [true, true, true],
-    };
-    const userinfo = toRef(store.state.user, "userinfo").value;
-    const {
-      "user/userChangeState": [userChangeState],
-    } = store._actions;
-    const state = reactive({
-      username: "",
-      password: "",
-    });
-    const onSubmit = (values) => {
-      values.action = "login";
-      userChangeState(values);
-    };
-    const haveIcon = computed(() => {
-      if (userinfo.userIcon === "") {
-        return false;
-      } else {
-        return true;
-      }
-    });
-    return {
-      state,
-      onSubmit,
-      haveIcon,
-    };
+    return {};
   },
 };
 </script>
 
 <style lang="scss">
-#login {
-  text-align: center;
+#findzjp {
   .van-image {
-    margin: 10px 0 15px 0;
-  }
-  .iconfont {
-    font-size: 8rem;
-  }
-  .halfbtn {
-    display: flex;
-    margin-top: 5px;
-    button {
-      flex: 1;
-      &:first-child {
-        border-top-left-radius: 999px;
-        border-bottom-left-radius: 999px;
-        margin-right: 1px;
-      }
-      &:last-child {
-        border-top-right-radius: 999px;
-        border-bottom-right-radius: 999px;
-        margin-left: 1px;
-      }
-    }
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
